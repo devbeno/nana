0a167df6318d43212aa932f8c90ad090b8091b88
Ben <benjophp@gmail.com>
2024-02-08 21:17:23 +0400

    init project

---
+++ src/app/site/page.tsx
@@ -0,0 +1,99 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { pricingCards } from "@/lib/constants";
+import clsx from "clsx";
+import { Check, Divide } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <>
+      <section className="h-full w-full pt-36 relative flex items-center justify-center flex-col">
+        {/* grid */}
+
+        <div className="absolute bottom-0 left-0 right-0 top-0 bg-[linear-gradient(to_right,#161616_1px,transparent_1px),linear-gradient(to_bottom,#161616_1px,transparent_1px)] bg-[size:4rem_4rem] [mask-image:radial-gradient(ellipse_60%_50%_at_50%_0%,#000_70%,transparent_110%)] -z-10" />
+        <p className="text-center">Run your agency, in one place</p>
+        <div className="bg-gradient-to-r from-primary to-secondary-foreground text-transparent bg-clip-text relative">
+          <h1 className="text-9xl font-bold text-center md:text-[300px]">
+            Nana
+          </h1>
+          <div className="flex justify-center items-center relative md:mt-[-70px]">
+            <Image
+              src={"/assets/preview.png"}
+              alt="banner img"
+              height={1200}
+              width={1200}
+              className="rounded-tl-2xl rounded-tr-2xl border-2 border-muted"
+            />
+            <div className="bottom-0 top-[50%] bg-gradient-to-t dark:from-background left-0 right-0 absolute z-10"></div>
+          </div>
+        </div>
+      </section>
+      <section className="flex justify-center items-center flex-col gap-4 mt-20">
+        <h2 className="text-4xl text-center">Choose what fits your right</h2>
+        <p className="text-muted-foreground text-center">
+          Our straightforward pricing plans are tailored to meet your needs. If{" "}
+          {" you're"} not <br />
+          ready to commit you can get started for free.
+        </p>
+        <div className="flex justify-center gap-4 flex-wrap mt-6">
+          {pricingCards.map((card) => (
+            //WIP: Wire up free product from Stripe
+            <Card
+              key={card.title}
+              className={clsx("w-[300px] flex flex-col justify-between", {
+                "border-2 border-primary": card.title === "Unlimited Sass",
+              })}
+            >
+              <CardHeader>
+                <CardTitle
+                  className={
+                    (clsx(""),
+                    {
+                      "text-muted-foreground": card.title !== "Unlimited Sass",
+                    })
+                  }
+                >
+                  {card.title}
+                </CardTitle>
+                <CardDescription>{card.description}</CardDescription>
+              </CardHeader>
+              <CardContent>
+                <span className="text-4xl font-bold">{card.price}</span>
+                <span className="text-muted-foreground">/m</span>
+              </CardContent>
+              <CardFooter className="flex flex-col item-start gap-4">
+                <div>
+                  {card.features.map((feature) => (
+                    <div key={feature} className="flex gap-2 item-center">
+                      <Check className="text-muted-foreground" />
+                      <p>{feature}</p>
+                    </div>
+                  ))}
+                </div>
+                <Link
+                  href={`/agency?plan=${card.priceId}`}
+                  className={clsx(
+                    "w-full text-center bg-primary p-2 rounded-md",
+                    {
+                      "!bg-muted-foreground": card.title !== "Unlimited Saas",
+                    }
+                  )}
+                >
+                  Get started
+                </Link>
+              </CardFooter>
+            </Card>
+          ))}
+        </div>
+      </section>
+    </>
+  );
+}
