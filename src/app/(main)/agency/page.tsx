2dba3df6730a4ea428e87ffdf702079e30715d51
Ben <benjophp@gmail.com>
2024-02-09 13:07:12 +0400

    queries update

--- src/app/(main)/agency/page.tsx
+++ src/app/(main)/agency/page.tsx
@@ -1,6 +1,15 @@
+import { getAuthUserDetails, verifyAndAcceptInvitation } from "@/lib/queries";
+import { currentUser } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
 import React from "react";
 
-const Page = () => {
+const Page = async () => {
+  const agencyId = await verifyAndAcceptInvitation();
+  console.log(agencyId);
+  //get user details
+
+  const user = await getAuthUserDetails();
+
   return <div>Agency </div>;
 };
 
